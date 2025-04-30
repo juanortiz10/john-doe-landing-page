07a8ae06a8ba72b1ba23e718274ff3015ec1bd54
Alfonso Ríos <malforime@gmail.com>
2025-04-30 23:40:20 +0000

    Removing duplicate path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/john-doe-landing-page/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
